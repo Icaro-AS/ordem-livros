596eddf66061a2ac266b67a4d35ade17b090d1f2
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-11-21 12:04:59 -0300

    Criação das rotas iniciais

---
+++ src/routes/autores/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-3xl font-bold">Autores</h1>
+<a href="/autores/george-martin">George Martin</a>
+<a href="/autores/tolkien">J.R.R Tolkien</a>
+<a href="/autores/jk-rowling">J.K Rowlin</a>
