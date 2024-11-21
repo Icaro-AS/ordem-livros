d395112384ed0a6b235dd78c546164b5e504bc65
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-11-21 10:20:12 -0300

    Criado o componente Logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Logo from "../components/template/Logo.svelte";
+
+</script>
+<div
+    class="flex flex.col justify-center items-center gap-5
+    bg-black text-white min-h-screen"
+>
+    <Logo></Logo>
+    <h1 class="text-3xl font-bold">Ordem dos Livros</h1>
+</div>
