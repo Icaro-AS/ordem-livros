72748769b6ceba56a713fc8e93b4937a16ab34f2
Ícaro Alencar Sousa <ias_epf@hotmail.com>
2024-11-22 09:50:25 -0300

    Compondo a página principal a partir dos componentes criados.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import Pagina from "../components/template/Pagina.svelte";
 	let { children } = $props();
-</script>
-
-{@render children()}
+  </script>
+  
+  <Pagina>
+	{@render children()}
+  </Pagina>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,2 @@
-<script>
-    import Logo from "../components/template/Logo.svelte";
-
-</script>
-<div
-    class="flex flex.col justify-center items-center gap-5
-    bg-black text-white min-h-screen"
->
-    <Logo></Logo>
-    <h1 class="text-3xl font-bold">Ordem dos Livros</h1>
-</div>
+<h1 class="text-3xl font-bold">Ordem dos livros</h1>
+<a href="/autores">Autores</a>
